712be810051a4173421af5c78d359c8c496924ca
Rajip2001 <Rajiptuitui272@gmail.com>
2024-09-06 12:54:57 +0545

    updated admin

--- todos/admin.py
+++ todos/admin.py
@@ -1,10 +1,4 @@
-# todos/admin.py
 from django.contrib import admin
-from .models import ToDo
-
-@admin.register(ToDo)
-class ToDoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'completed', 'created_at')
-    list_filter = ('completed', 'created_at')
-    search_fields = ('title', 'description')
-
+from todos.models import TODO
+# Register your models here.
+admin.site.register(TODO)
